93122ff7d45673cb95c1625bdd97afcac687d2c5
Tue S. Dissing <tue@somersault.dk>
2013-10-29 11:16:46 +0100

    adding reader concept.

---
+++ mopidy_arcam/reader.py
@@ -0,0 +1,37 @@
+'''
+Created on 29/10/2013
+
+@author: tue
+'''
+
+
+import logging
+import pykka
+
+from findertools import sleep
+
+logger = logging.getLogger('mopidy_arcam')
+
+
+class ArcamReader(pykka.ThreadingActor):
+    """
+    Independent reader thread which does the communication with the Arcam amplifier.
+    
+    """
+    _read_lines = ""
+    
+    def __init__(self):
+        super(ArcamReader, self).__init__()
+        
+    def on_start(self, talker):
+        self._arcam_talker = talker
+        
+        # Let's start reading
+        while (True):
+            logger.info("Starting to read.")
+            self._read_lines = self._arcam_talker.readline()
+            if (len(self._read_lines) > 0):
+                print "Read: ", self._read_lines
+            else:
+                print "Nothing yet."
+                sleep(5)
